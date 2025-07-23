d3433b4a727a44a670aab24e09d4e11a44b52d26
dvthang774 <74769024+dvthang774@users.noreply.github.com>
2025-07-23 10:23:50 +0700

    update pages

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '*2izf_rnatia8yzrw&w_1()*0thnmwl1x%b#um)(04azi7*-x&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    h for h in os.getenv('ALLOWED_HOSTS', '').split(",") if h
+]
 
 
 # Application definition
